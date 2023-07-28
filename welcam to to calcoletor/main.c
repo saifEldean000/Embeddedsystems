2db1d51619737e47b0699dfeddaddfd658caabb6
saifEldean000 <118446389+saifEldean000@users.noreply.github.com>
2023-07-28 19:47:57 +0300

    welcam to calcoletor

--- welcam to to calcoletor/main.c
+++ welcam to to calcoletor/main.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 int main() {
-    int  x = 0 ;
-    int  y = 0 ;
+    float x = 0 ;
+    float y = 0 ;
     int op ;
-    printf("****welcome to to calculator****\n");
+    printf("****welcome to calculator****\n");
     while  (1){
-    printf("enter the num 1 =");
-    scanf("%d",&x);
-    printf("enter the num 2 =");
-    scanf("%d",&y);
+    printf("enter the first number  =");
+    scanf("%f",&x);
+    printf("enter the second number =");
+    scanf("%f",&y);
     printf("1=(+)\n2=(-)\n3=(*)\n4=(/)\nEnter the operator =");
     int num_matched = scanf(" %d", &op);
 
     if (num_matched == 1 && op == 1)
     {
-        printf("=%d\n",x+y);
+        printf("=%f\n",x+y);
     }
     else if (num_matched == 1 && op == 2)
     {
-        printf("=%d\n",x-y);
+        printf("=%f\n",x-y);
     }
     else if (num_matched == 1 && op == 4)
     {
-        printf("=%d\n",x/y);
+        printf("=%f\n",x/y);
     }
     else if (num_matched == 1 && op == 3)
     {
-        printf("=%d\n",x*y);
+        printf("=%f\n",x*y);
     }
     else {
         printf("Error:\n");
